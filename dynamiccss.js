2bc47e90cc2bc994988cd613a0b9304970ce3211
weitaoyap111 <weitaoyap111@gmail.com>
2024-04-06 19:30:16 +0800

    version 1.0.0

---
+++ dynamiccss.js
@@ -0,0 +1,85 @@
+const separator = "=>"
+const header = "dcj"
+const header_pos = 0
+const elements_pos = 1
+const header_element = 3
+
+// get unique class 
+function get_unique_class_list(){
+	let all_classes = [];
+	let all_elements = document.querySelectorAll('*');
+
+	for (let i = 0; i < all_elements.length; i++) {
+		let classes = all_elements[i].className.toString().split(/\s+/);
+		for (let j = 0; j < classes.length; j++) {
+			let cls = classes[j];
+			if (cls && all_classes.indexOf(cls) === -1) all_classes.push(cls);
+		}
+	}
+	return all_classes;
+}
+
+// change class name into list
+function decode_class_name(class_name){
+	return class_name.toString().split(separator);
+}
+
+// check class is exist or not
+function check_class_is_exist(class_name){
+	let get_element_by_class = document.getElementsByClassName(class_name);
+	return (get_element_by_class.length 
+		>= 1 ? true: false);
+}
+
+// auto router function
+function router_function(class_name){
+	if(check_class_is_exist(class_name)){
+		let class_elements = document.getElementsByClassName(class_name);
+		let css_atr_list = []
+		for(let i = 0; i<class_elements.length;i++){
+			css_atr_list = decode_class_name(class_name);
+			if(css_atr_list[header_pos] == header){
+				console.log(css_atr_list.length, css_atr_list);
+				if(css_atr_list.length > header_element){
+					CssMultipleInput(class_elements[i], css_atr_list)
+				}
+				else{
+					CssSingleInput(class_elements[i], css_atr_list)
+				}
+			}
+		}
+	}
+}
+
+// main function
+function main(){
+	// main function
+	let class_lists = get_unique_class_list();
+	for(let i = 0; i < class_lists.length; i++){
+		router_function(class_lists[i]);
+	}
+}
+
+// core 1 (single input)
+function CssSingleInput(class_element, list_data){
+	class_element.style[list_data[elements_pos]] = list_data[elements_pos+1];
+}
+
+// core 2 (multiple input)
+function CssMultipleInput(class_element, list_data){
+	let data_string = "";
+	for(let i = 0; i < list_data.length; i++){
+		if(i > header_pos){
+			if(i+1 < list_data.length){
+				data_string += list_data[i+1];
+			}
+			if(i+1 < list_data.length - 1){
+				data_string += " "
+			}
+		}
+	}
+	console.log(data_string);
+	class_element.style[list_data[elements_pos]] = data_string;
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,40 @@
+
+# Dynamic Css javascript
+
+This project aim is to create styling using javascript without using css. It return in pure javascript and no external library is used.
+
+## How to install it
+
+put this line at your html.
+```
+<script type="text/javascript" src="dynamiccss.js"></script>
+```
+
+## How to use it
+
+pattern of the classname:
+```
+dcj=>[css attribute name]=>[css option 1]=>[css option 2]=>...=>[css option n] 
+```
+note: n is number of option in that css attribute.
+
+Example: 
+in css 
+```
+div {
+    color: blue
+}
+```
+
+in html
+```
+<div class="dcj=>color=>blue"></div>
+```
+
+Your can refer to the index.html look for more example.
+
+# Donate 
+
+If useful for you and you like this, kindly consider a donation
+
+[Donate](https://paypal.me/weitaoyap?country.x=MY&locale.x=en_US)
